223e9407130091d5274746f95b17e0684c5e9781
Zara Patten <zara@patten.io>
2023-02-08 06:26:47 -0600

    blargh

--- lib/servers/actions.rb
+++ lib/servers/actions.rb
@@ -7,17 +7,17 @@ class Servers
 
     def start!(container: false)
       LinkLogger.info(:servers) { "Start Servers (container: #{container.ai})" }
-      each { |server| Runner.pool.post { server.start!(container: container) }; sleep 3 }
+      select(&:container_dead?).each { |server| Runner.pool.post { server.start!(container: container) }; sleep 3 }
     end
 
     def stop!(container: false)
       LinkLogger.warn(:servers) { "Stopping Servers (container: #{container.ai})" }
-      each { |server| Runner.pool.post { server.stop!(container: container) } }
+      select(&:container_alive?).each { |server| Runner.pool.post { server.stop!(container: container) } }
     end
 
     def restart!(container: false)
       LinkLogger.warn(:servers) { "Restart Servers (container: #{container.ai})" }
-      each { |server| Runner.pool.post { server.restart!(container: container) } }
+      select(&:container_alive?).each { |server| Runner.pool.post { server.restart!(container: container) } }
     end
 
 ################################################################################
